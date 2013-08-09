27e3af8f432d340c3966aad4152a7b7107a6f90f
cgray <cgray@yammer-inc.com>
2013-08-08 18:25:07 -0700

    configuration view and post resource to submit new tenacity configuration

--- breakerbox-service/src/main/java/com/yammer/breakerbox/service/resources/ConfigureResource.java
+++ breakerbox-service/src/main/java/com/yammer/breakerbox/service/resources/ConfigureResource.java
@@ -3,17 +3,55 @@ package com.yammer.breakerbox.service.resources;
 import com.yammer.breakerbox.service.core.ServiceId;
 import com.yammer.breakerbox.service.views.ConfigureView;
 import com.yammer.metrics.annotation.Timed;
+import com.yammer.tenacity.core.config.CircuitBreakerConfiguration;
+import com.yammer.tenacity.core.config.TenacityConfiguration;
+import com.yammer.tenacity.core.config.ThreadPoolConfiguration;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/configure/{service}")
 public class ConfigureResource {
     @GET @Timed @Produces(MediaType.TEXT_HTML)
     public ConfigureView render(@PathParam("service") String serviceName) {
         return new ConfigureView(ServiceId.from(serviceName));
+    }
+
+
+    @POST @Timed @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Path("/{dependency}/{environment}")
+    public Response configure(@PathParam("service") String serviceName,
+                              @PathParam("dependency") String dependencyName,
+                              @PathParam("environment") String environmentName,
+                              @FormParam("executionTimeout") Integer executionTimeout,
+                              @FormParam("requestVolumeThreshold") Integer requestVolumeThreshold,
+                              @FormParam("errorThresholdPercentage") Integer errorThresholdPercentage,
+                              @FormParam("sleepWindow") Integer sleepWindow,
+                              @FormParam("circuitBreakerstatisticalWindow") Integer circuitBreakerstatisticalWindow,
+                              @FormParam("circuitBreakerStatisticalWindowBuckets") Integer circuitBreakerStatisticalWindowBuckets,
+                              @FormParam("threadPoolCoreSize") Integer threadPoolCoreSize,
+                              @FormParam("keepAliveMinutes") Integer keepAliveMinutes,
+                              @FormParam("maxQueueSize") Integer maxQueueSize,
+                              @FormParam("queueSizeRejectionThreshold") Integer queueSizeRejectionThreshold,
+                              @FormParam("threadpoolStatisticalWindow") Integer threadpoolStatisticalWindow,
+                              @FormParam("threadpoolStatisticalWindowBuckets") Integer threadpoolStatisticalWindowBuckets) {
+        final TenacityConfiguration tenacityConfiguration = new TenacityConfiguration(
+                new ThreadPoolConfiguration(
+                        threadPoolCoreSize,
+                        keepAliveMinutes,
+                        maxQueueSize,
+                        queueSizeRejectionThreshold,
+                        threadpoolStatisticalWindow,
+                        threadpoolStatisticalWindowBuckets),
+                new CircuitBreakerConfiguration(
+                        requestVolumeThreshold,
+                        sleepWindow,
+                        errorThresholdPercentage,
+                        circuitBreakerstatisticalWindow,
+                        circuitBreakerStatisticalWindowBuckets),
+                executionTimeout);
+        return Response.ok().build();
+
     }
 }
