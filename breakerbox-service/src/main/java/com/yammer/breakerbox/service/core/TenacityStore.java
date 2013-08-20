6b4fc191d6ea72132bd6ec5d84c2714ce08d7181
cgray <cgray@yammer-inc.com>
2013-08-19 17:19:22 -0700

    Simple cache around listing dependencies from Azure Table. Invalidates when a service dependencies change.

--- breakerbox-service/src/main/java/com/yammer/breakerbox/service/core/TenacityStore.java
+++ breakerbox-service/src/main/java/com/yammer/breakerbox/service/core/TenacityStore.java
@@ -1,6 +1,8 @@
 package com.yammer.breakerbox.service.core;
 
 import com.google.common.base.Optional;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import com.google.common.collect.ImmutableList;
 import com.microsoft.windowsazure.services.table.client.TableConstants;
 import com.microsoft.windowsazure.services.table.client.TableQuery;
@@ -12,12 +14,23 @@ import com.yammer.metrics.Metrics;
 import com.yammer.metrics.core.Timer;
 import com.yammer.metrics.core.TimerContext;
 import com.yammer.tenacity.core.config.TenacityConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 public class TenacityStore {
     private final TableClient tableClient;
+    private final Cache<ServiceId, ImmutableList<ServiceEntity>> listDependenciesCache = CacheBuilder
+            .newBuilder()
+            .expireAfterWrite(5, TimeUnit.MINUTES)
+            .build();
 
     private static final Timer LIST_SERVICES = Metrics.newTimer(TenacityStore.class, "list-services");
     private static final Timer LIST_SERVICE = Metrics.newTimer(TenacityStore.class, "list-service");
+    private static final Logger LOGGER = LoggerFactory.getLogger(TenacityStore.class);
 
     public TenacityStore(TableClient tableClient) {
         this.tableClient = tableClient;
@@ -32,6 +45,7 @@ public class TenacityStore {
     }
 
     public boolean store(ServiceEntity serviceEntity) {
+        listDependenciesCache.invalidate(serviceEntity.getServiceId());
         return tableClient.insertOrReplace(serviceEntity);
     }
 
@@ -47,8 +61,18 @@ public class TenacityStore {
         return allServiceEntities();
     }
 
-    public ImmutableList<ServiceEntity> listDependencies(ServiceId serviceId) {
-        return allServiceEntities(serviceId);
+    public ImmutableList<ServiceEntity> listDependencies(final ServiceId serviceId) {
+        try {
+            return listDependenciesCache.get(serviceId, new Callable<ImmutableList<ServiceEntity>>() {
+                @Override
+                public ImmutableList<ServiceEntity> call() throws Exception {
+                    return allServiceEntities(serviceId);
+                }
+            });
+        } catch (ExecutionException err) {
+            LOGGER.warn("Could not fetch dependencies for {}", serviceId, err);
+        }
+        return ImmutableList.of();
     }
 
     private ImmutableList<ServiceEntity> allServiceEntities(ServiceId serviceId) {
