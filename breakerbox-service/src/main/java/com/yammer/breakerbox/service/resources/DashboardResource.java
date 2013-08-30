33d5f0a79d146ed2128ea471c1b78d8d649a2be5
cgray <cgray@yammer-inc.com>
2013-08-30 14:54:13 -0700

    default to production

--- breakerbox-service/src/main/java/com/yammer/breakerbox/service/resources/DashboardResource.java
+++ breakerbox-service/src/main/java/com/yammer/breakerbox/service/resources/DashboardResource.java
@@ -3,16 +3,13 @@ package com.yammer.breakerbox.service.resources;
 import com.yammer.breakerbox.service.views.DashboardView;
 import com.yammer.metrics.annotation.Timed;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 @Path("/")
 public class DashboardResource {
     @GET @Timed @Produces(MediaType.TEXT_HTML)
-    public DashboardView render(@QueryParam("cluster") String clusterName) {
+    public DashboardView render(@DefaultValue("production") @QueryParam("cluster") String clusterName) {
         return new DashboardView(clusterName);
     }
 }
