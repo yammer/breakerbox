81dc30055c803a3258d2449c46497a2ff7e7e7e6
cgray <cgray@yammer-inc.com>
2013-09-11 15:25:18 -0700

    Dashboard doesn't need LDAP.

--- breakerbox-service/src/main/java/com/yammer/breakerbox/service/resources/DashboardResource.java
+++ breakerbox-service/src/main/java/com/yammer/breakerbox/service/resources/DashboardResource.java
@@ -1,12 +1,6 @@
 package com.yammer.breakerbox.service.resources;
 
-import com.google.common.base.Optional;
 import com.yammer.breakerbox.service.views.DashboardView;
-import com.yammer.dropwizard.auth.Auth;
-import com.yammer.dropwizard.auth.AuthenticationException;
-import com.yammer.dropwizard.auth.Authenticator;
-import com.yammer.dropwizard.auth.basic.BasicCredentials;
-import com.yammer.dropwizard.authenticator.LdapAuthenticator;
 import com.yammer.metrics.annotation.Timed;
 
 import javax.ws.rs.*;
@@ -15,8 +9,7 @@ import javax.ws.rs.core.MediaType;
 @Path("/")
 public class DashboardResource {
     @GET @Timed @Produces(MediaType.TEXT_HTML)
-    public DashboardView render(@Auth BasicCredentials credentials,
-            @DefaultValue("production") @QueryParam("cluster") String clusterName) {
+    public DashboardView render(@DefaultValue("production") @QueryParam("cluster") String clusterName) {
         return new DashboardView(clusterName);
     }
 }
