0354c1348b23e78afd7865135442df89a10dcf0f
cgray <cgray@yammer-inc.com>
2015-04-03 13:05:36 -0700

    don't read in the index.html as a file, instead serve use a uber-jar friendly way of reading the file

--- breakerbox-dashboard/src/main/java/com/yammer/breakerbox/dashboard/resources/IndexResource.java
+++ breakerbox-dashboard/src/main/java/com/yammer/breakerbox/dashboard/resources/IndexResource.java
@@ -1,21 +1,36 @@
 package com.yammer.breakerbox.dashboard.resources;
 
 import com.codahale.metrics.annotation.Timed;
-import com.google.common.base.Charsets;
-import com.google.common.io.Files;
+import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
 import com.google.common.io.Resources;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import java.io.File;
+import java.io.IOException;
 
 @Path("/tenacity")
 public class IndexResource {
+    private final Supplier<byte[]> indexSupplier;
+
+    public IndexResource() {
+        indexSupplier = Suppliers.memoize(new Supplier<byte[]>() {
+            @Override
+            public byte[] get() {
+                try {
+                    return Resources.asByteSource(Resources.getResource("assets/index.html"))
+                            .read();
+                } catch (IOException err) {
+                    throw new IllegalStateException(err);
+                }
+            }
+        });
+    }
+
     @GET @Timed @Produces(MediaType.TEXT_HTML)
-    public Response render() throws Exception {
-        return Response.ok(Files.toString(new File(Resources.getResource("index.html").toURI()), Charsets.UTF_8)).build();
+    public byte[] render() throws IOException {
+        return indexSupplier.get();
     }
 }
