2fb2fafc6e9b425d517527d523d09d49b90f4159
cgray <cgray@yammer-inc.com>
2015-11-04 11:28:42 -0800

    Dropwizard 0.9.1 Tenacity 0.7.0 (in progress) dropwizard-auth-ldap 0.3.0 Upgrading maven-plugins Postgresql 9.4-1205-jdbc42 H2 1.4.190

--- breakerbox-service/src/main/java/com/yammer/breakerbox/service/auth/NullAuthenticator.java
+++ breakerbox-service/src/main/java/com/yammer/breakerbox/service/auth/NullAuthenticator.java
@@ -1,13 +1,16 @@
 package com.yammer.breakerbox.service.auth;
 
 import com.google.common.base.Optional;
+import com.yammer.dropwizard.authenticator.User;
 import io.dropwizard.auth.AuthenticationException;
 import io.dropwizard.auth.Authenticator;
 import io.dropwizard.auth.basic.BasicCredentials;
 
-public class NullAuthenticator implements Authenticator<BasicCredentials, BasicCredentials> {
+import java.util.Collections;
+
+public class NullAuthenticator implements Authenticator<BasicCredentials, User> {
     @Override
-    public Optional<BasicCredentials> authenticate(BasicCredentials credentials) throws AuthenticationException {
-        return Optional.of(new BasicCredentials("anonymous", ""));
+    public Optional<User> authenticate(BasicCredentials credentials) throws AuthenticationException {
+        return Optional.of(new User("anonymous", Collections.<String>emptySet()));
     }
 }
